b5dc299d2f77970fe1d26f8f86cc4e8b389437d9
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-11-12 12:30:49 +0530

    header and paragraph section work done

---
+++ src/form/SignUpForm.jsx
@@ -0,0 +1,10 @@
+export const SignUpForm =()=>{
+    return <div className="border flex justify-center items-center h-screen w-full">
+        <div className="border w-96 h-auto flex justify-center items-center shadow-lg flex-col shadow-cyan-700" >
+
+        <h1 className="text-blue-700 text-2xl font-bold ">Sign UP </h1>
+        <span><p className="font-extralight font-thin text-neutral-400">Please Fill in this form to create an account </p></span>
+       
+        </div>
+    </div>
+}
