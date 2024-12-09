e1ce5b0885abe19d78893b85853b3088aebcccff
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-12-09 17:08:17 +0530

    leaning form handling in react and how to pass data to the backend

--- src/form/LogInForm.jsx
+++ src/form/LogInForm.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "../index.css";
 export const LogInForm = () => {
+  const [user, setUser] = useState({
+    userName: "",
+    pass: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <div className=" flex justify-center items-center h-screen w-full">
       <form
         className="border w-96 h-auto flex justify-center p-3 shadow-lg flex-col shadow-cyan-700"
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
       >
         <h1 className="text-blue-700 text-5xl font-bold my-1 mb-2">Log In </h1>
 
@@ -13,6 +28,7 @@ export const LogInForm = () => {
           type="text"
           name="userName"
           id="UserName"
+          onChange={handleChange}
           placeholder="Enter Your userName"
           className="p-1 rounded-md bg-slate-200 "
         />
@@ -22,6 +38,7 @@ export const LogInForm = () => {
           type="password"
           name="pass"
           id="pass"
+          onChange={handleChange}
           placeholder="Enter your password"
           className="p-1 rounded-md bg-slate-200 "
         />
